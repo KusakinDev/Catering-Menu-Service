2b2d9a6261df8c1c6259c846494f5818d5a1ae35
alexander kusakin <pm12.kusakin@gmail.com>
2025-03-21 18:58:17 +0700

    ./migrate/: Add new scripts

--- migrate/source/migrate_all_models/main.go
+++ migrate/source/migrate_all_models/main.go
@@ -6,6 +6,7 @@ import (
 	allergytypemodel "github.com/KusakinDev/Catering-Menu-Service/internal/models/allergy_type_model"
 	categorymodel "github.com/KusakinDev/Catering-Menu-Service/internal/models/category_model"
 	dishmodel "github.com/KusakinDev/Catering-Menu-Service/internal/models/dish_model"
+	nutritionfactmodel "github.com/KusakinDev/Catering-Menu-Service/internal/models/nutrition_fact_model"
 	tagmodel "github.com/KusakinDev/Catering-Menu-Service/internal/models/tag_model"
 	typemodel "github.com/KusakinDev/Catering-Menu-Service/internal/models/type_model"
 )
@@ -27,7 +28,7 @@ func main() {
 	var cat categorymodel.Category
 	cat.MigrateToDB(db)
 
-	var nutFuct dishmodel.Dish
+	var nutFuct nutritionfactmodel.NutritionFact
 	nutFuct.MigrateToDB(db)
 
 	var tag tagmodel.Tag
