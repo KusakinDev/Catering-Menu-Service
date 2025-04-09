bd80e3fb00e055284af277440e62f14b38c85d99
alexander kusakin <pm12.kusakin@gmail.com>
2025-04-09 18:50:15 +0700

    ./cmd/main.go: fix grp port

--- cmd/main.go
+++ cmd/main.go
@@ -34,7 +34,7 @@ func main() {
 
 	// gRPC
 	go func() {
-		listener, err := net.Listen("tcp", ":50051")
+		listener, err := net.Listen("tcp", ":50052")
 		if err != nil {
 			log.Fatalf("Failed to listen: %v", err)
 		}
@@ -42,7 +42,7 @@ func main() {
 		grpcServer := grpc.NewServer()
 		pb.RegisterDishServiceServer(grpcServer, &server.Server{})
 
-		log.Println("gRPC server is running on port :50051")
+		log.Println("gRPC server is running on port :50052")
 		if err := grpcServer.Serve(listener); err != nil {
 			log.Fatalf("Failed to serve gRPC server: %v", err)
 		}
